1cc9a298e55c15c30ab07cddcb1f5ed24b622ec7
Harish Mekwana <Harish.Mekwana@lh.org.au>
2021-09-22 17:51:36 +1000

    Front End creation in Progress

---
+++ TAL/TAL/src/app/_models/rating.ts
@@ -0,0 +1,6 @@
+export enum Rating {
+    Light_Manual = 'Light Manual',
+    Professional = 'Professional',
+    White_Collar = 'White Collar',
+    Heavy_Manual = 'Heavy Manual'
+}
---
+++ TAL/TAL/src/app/_models/user.ts
@@ -0,0 +1,14 @@
+﻿// import { Role } from './role';
+import { Rating } from './rating';
+import { Occupation } from './occupation';
+
+export class User {
+    id!: string;
+    title!: string;
+    firstName!: string;
+    lastName!: string;
+    email!: string;
+    occupation!: Occupation;
+    rating!: Rating;    
+    isDeleting: boolean = false;
+}
